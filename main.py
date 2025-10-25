d2350b7f46c500e4369104a90d6cfd09a7dbc4e6
Your Name <your-email@example.com>
2025-10-25 16:48:17 -0400

    Added CORS Middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from model.database import DBSession
 from model import models
 from schemas import NoteInput
@@ -6,6 +7,18 @@ from datetime import datetime
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173" # React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # Returns all notes.
 @app.get("/notes")
